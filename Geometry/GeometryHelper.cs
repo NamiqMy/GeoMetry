69a517ad50b3861208f77dfbf6af8eb17441cb01
NamiqMy <namiqmy@code.edu.az>
2023-06-10 01:17:21 +0400

    Creating Geometry project

---
+++ Geometry/GeometryHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometry
+{
+    public static class GeometryHelper
+    {
+        public static double CalculateRectangleArea(double length, double width)
+        {
+            return length * width;
+        }
+
+        public static double CalculateTriangleArea(double baselength, double height)
+        {
+            return baselength * height / 2;
+        }
+        public static double CalculateCircleArea(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+
+        
+    }
+}
---
+++ Geometry/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Geometry
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double rectangleArea = GeometryHelper.CalculateRectangleArea(4, 6);
+            Console.WriteLine("Dörtgenin alanı: " + rectangleArea);
+
+            double triangleArea = GeometryHelper.CalculateTriangleArea(3, 5);
+            Console.WriteLine("Üçgenin alanı: " + triangleArea);
+
+            double circleArea = GeometryHelper.CalculateCircleArea(2);
+            Console.WriteLine("Dairenin alanı: " + circleArea);
+
+            Console.ReadLine();
+
+        }
+    }
+}
